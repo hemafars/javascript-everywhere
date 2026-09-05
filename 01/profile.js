d19e8b65afbe81c8ef7cbcda27a0a1f6a824a2df
hemafars <hemafars0@gmail.com>
2026-09-05 18:12:59 +0300

    add img

---
+++ 01/profile.js
@@ -0,0 +1,10 @@
+let myName = "hemaf fars";
+let myCity = " geza";
+let whyIJoined = "learn frontEnd";
+
+function statment() {
+  console.log(
+    `myName : ${myName} , myCity : ${myCity} , whyIJoined : ${whyIJoined}` ,
+  );
+}
+statment();
