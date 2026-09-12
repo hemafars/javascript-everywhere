c25106418291716d20e3bc01e803ce0cb477c7e3
hemafars <hemafars0@gmail.com>
2026-09-12 12:27:37 +0300

    add img  2.1

---
+++ day-02/types.js
@@ -0,0 +1,35 @@
+let myName = "hemafars";
+let age = 40;
+let isEgyption = true;
+let employ = null;
+let outDate;
+
+let person = {
+  filrstName: "hema",
+  lastName: "fars",
+};
+
+let student = ["ahmed", "mohamed", "ali"];
+
+console.log(`${student[0]}`);
+
+console.log(
+  `${myName} → ${typeof myName}
+  ${age} → ${typeof age}
+  ${isEgyption} → ${typeof isEgyption}
+  ${employ} → ${typeof employ}
+  ${outDate} → ${typeof outDate}
+  ${person} → ${typeof person}
+  ${person.filrstName} → ${typeof person.filrstName}
+  ${person.lastName} → ${typeof person.lastName}
+  ${student} → ${typeof student}
+  `,
+);
+
+console.log(
+  `${student[0]} → ${typeof student[0]}
+  ${student[1]} → ${typeof student[1]}
+  ${student[2]} → ${typeof student[2]}`,
+);
+
+//→
